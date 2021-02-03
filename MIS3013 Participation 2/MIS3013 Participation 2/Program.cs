f9d151f9ff56a2a3360e2868e56148e6882691b7
Alexander Upton <Alexander99W@ou.edu>
2021-02-02 18:43:10 -0600

    Save

--- MIS3013 Participation 2/MIS3013 Participation 2/Program.cs
+++ MIS3013 Participation 2/MIS3013 Participation 2/Program.cs
@@ -6,7 +6,25 @@ namespace MIS3013_Participation_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string firstName, lastName;
+            double gpa;
+
+
+            Console.WriteLine("Please enter your first name >>");
+            firstName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your lastname >>");
+            lastName = Console.ReadLine();
+
+            Console.WriteLine("Please enter your gpa >>");
+            string gpaAsString = Console.ReadLine();
+            gpa = Convert.ToDouble(gpaAsString);
+
+            gpa = gpa + .5; // i would really love it if they bumped my gpa up by .5
+
+            string output = "Hello " + firstName + " " + lastName + " " + " we boosted your gpa to " + gpa.ToString("N3");
+
+            Console.WriteLine(output);
         }
     }
 }
