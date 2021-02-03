8ce798947ef29968ebd23dcaa9f9eb93bb3611b8
Alexander Upton <Alexander99W@ou.edu>
2021-02-02 18:18:24 -0600

    Solution for participation

---
+++ MIS3013 Participation 2/MIS3013 Participation 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MIS3013_Participation_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
