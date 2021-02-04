4d0db09fe71711f72ae7d402024999a537049d89
Alexander Upton <Alexander99W@ou.edu>
2021-02-04 17:34:28 -0600

    I will turn a fella into a convertible

---
+++ participation 2-4-2021/participation 2-4-2021/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace participation_2_4_2021
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+               const double SUPER_NUMBER = 3.1415;
+
+            //SUPER_NUMBER = 69;
+
+        }
+    }
+}
