db184bb4b1008d88dceb7a7f92b18ca30907f53a
Alexander Upton <Alexander99W@ou.edu>
2021-02-09 17:52:55 -0600

    Not finished but saving anyway

--- participation 2-4-2021/participation 2-4-2021/Program.cs
+++ participation 2-4-2021/participation 2-4-2021/Program.cs
@@ -6,10 +6,28 @@ namespace participation_2_4_2021
     {
         static void Main(string[] args)
         {
-               const double SUPER_NUMBER = 3.1415;
+               const double MAGIC_NUMBER = 7.777;
 
             //SUPER_NUMBER = 69;
 
+            Console.WriteLine("Please enter the first number");
+            string answer = Console.ReadLine();
+            double num1 = Convert.ToDouble(answer);
+
+            Console.WriteLine("Please enter the second number");
+            answer = Console.ReadLine();
+            double num2 = Convert.ToDouble(answer);
+
+            Console.WriteLine("Please enter the third number");
+            answer = Console.ReadLine();
+            double num3 = Convert.ToDouble(answer);
+
+            Console.WriteLine($"{num1} + {num2} + {num3} = {(num1 + num2 + num3).ToString("N3")}");
+
+            double magicNumberTimesSum = magicNumberTimesSum * SUPER_NUMBER;
+
+
+            Console.WriteLine($"Your sum multiplied by our super number is {magicNumberTimesSum.ToString}");
         }
     }
 }
